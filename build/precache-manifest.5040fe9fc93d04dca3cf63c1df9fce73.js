b0fa114022933c2e76d3bee03f68ea370714ec71
manukkutty3090 <you@example.com>
2022-05-07 20:17:15 +0530

    Updated the app

---
+++ build/precache-manifest.5040fe9fc93d04dca3cf63c1df9fce73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96efad0c94b6042428ee0413ea025699",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "a78d2b608d184901fb75",
+    "url": "/Keeper-App/static/js/2.5e106bb2.chunk.js"
+  },
+  {
+    "revision": "387481af6d84e402f160",
+    "url": "/Keeper-App/static/js/main.88cb83cd.chunk.js"
+  },
+  {
+    "revision": "128fd620914684fbc76b",
+    "url": "/Keeper-App/static/js/runtime-main.113ea231.js"
+  }
+]);
